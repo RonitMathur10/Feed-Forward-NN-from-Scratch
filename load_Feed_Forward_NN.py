eef59c36ba5444afd0df254659f438909b0f9fce
Ronit <ronit.mathur@berkeley.edu>
2026-09-18 14:48:47 -0700

    Revert "finished code for saving trained models AND load_NN class --> can restore old models/analyze older models when needed. easier to create/save/load models"
    This reverts commit a4ab2e67159a89a16485edcbd087459a47b0a226.

--- load_Feed_Forward_NN.py
+++ load_Feed_Forward_NN.py
@@ -6,35 +6,14 @@ class load_NN(NN):
         if data_location == "Kaggle_Digits":
             data_path = "/Users/ronitmathur/Desktop/ML/Feed-Forward_NN/Data/Processed_Data/Kaggle_Digits"
 
-            X_train = np.load(f"{data_path}/X_train.npy")
-            Y_train = np.load(f"{data_path}/Y_train.npy")
-            X_validation = np.load(f"{data_path}/X_validation.npy")
-            Y_validation = np.load(f"{data_path}/Y_validation.npy")
-            self.X_test = np.load(f"{data_path}/X_test.npy").T
+            X_train = np.load(f"{data_path}/X_train")
+            Y_train = np.load(f"{data_path}/Y_train")
+            X_validation = np.load(f"{data_path}/X_validation")
+            Y_validation = np.load(f"{data_path}/Y_validation")
+            self.X_test = np.load(f"{data_path}/X_test")
         
-        saved_model_path = f"/Users/ronitmathur/Desktop/ML/Feed-Forward_NN/Kaggle/Results/{model_name}"
-        layer_sizes = np.load(f"{saved_model_path}/layer_sizes.npy")
-
-
-        data = np.load(f"{saved_model_path}/weights_and_biases.npz")
-        number_layers_for_wb = np.size(layer_sizes) - 1
-        
-        # When passed as positional arguments (*), arrays are named 'arr_0', 'arr_1', etc.
-        # Total positional items = w_count + b_count
-        all_arrays = [data[f'arr_{i}'] for i in range(2*number_layers_for_wb)]
-        
-        # Slice the combined list back into weights and biases
-        weights = all_arrays[:number_layers_for_wb]
-        biases = all_arrays[number_layers_for_wb:]
-        
-        
-        super().__init__(model_name, X_train.T, Y_train.T, X_validation.T, Y_validation.T, layer_sizes[1:-1])
-        self.set_loaded_weigths_and_biases(weights, biases)
-
-
-        print ("Model Loaded")
+        super.__init__(model_name, X_train, Y_train, X_validation, Y_validation)
 
         # DO:
+        #   1. add code to save 1. layer sizes , 2. model's weights & biases
         #   2. create setter function in class.py to set weights & biases
-        #   3. create system to save trained models into respective folders
-        # ---- EVERYTHING DONE I THINK, JUST CHECK WITH TESTING. also replace code in notebook to just load the kaggle data instead of calc. everything again ----
